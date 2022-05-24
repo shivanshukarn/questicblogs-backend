fe3b5faa22c4fd6c9314dafaa3fa0f7cfa567e28
Shivanshu Karn <karnshivanshu2005@gmail.com>
2022-05-24 09:10:20 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "questicblogstest"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "shivanshu"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
