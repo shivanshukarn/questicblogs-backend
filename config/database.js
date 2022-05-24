bcc1a96de1072c741ad5eb0a9a870c12ef4c27b1
Shivanshu Karn <karnshivanshu2005@gmail.com>
2022-05-24 10:26:08 +0530

    initial commit 5A

--- config/database.js
+++ config/database.js
@@ -9,6 +9,7 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "shivanshu"),
       schema: env("DATABASE_SCHEMA", "public"),
+      ssl: { rejectUnauthorized: false },
     },
   }
 });
